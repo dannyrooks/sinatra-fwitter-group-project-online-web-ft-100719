f127ad3cd4253732ec52ef81d883fe68af601ad6
Daniel Rooker <dannyrooks@gmail.com>
2019-12-04 15:22:02 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_202903) do
+ActiveRecord::Schema.define(version: 2019_12_04_164034) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
